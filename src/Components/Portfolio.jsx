9711a27fb7e38a76bd4c71ae80908b2e3108dbb8
Lipun <lipundani@gmail.com>
2023-12-01 22:39:38 +0530

    updated images

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/desk-of-tech.avif";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Desk filled up with laptop, phone, stationary and coffee";
 
 /**
  * Project list
